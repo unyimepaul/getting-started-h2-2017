5ca55f7b875ed50b0f08125315191028f430bd18
Tim <fabrobocomx@gmail.com>
2017-08-21 22:56:24 +0100

    Added Name to contributors, resolved conflict and removed 'dupi530p.3dd'

--- hnginternship.php
+++ hnginternship.php
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-
-
 <?php
+
 $servername = "127.0.0.1";
 $username = "root";
 $password = "";
@@ -15,21 +13,3 @@ if ($conn->connect_error) {
 }
 echo "Connection OK!";
 ?>
-=======
-
-
-<?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-
-// Create connection
-$conn = new mysqli('127.0.0.1','root', '');
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connection OK!";
-?>
->>>>>>> f3c1a9f6bc4e22fccc0d28ca15d8d68531ad954a
